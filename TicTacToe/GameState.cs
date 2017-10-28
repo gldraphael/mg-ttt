220d7aca3de76123e3b0a8a3c1643c1651596b86
Galdin Raphael <gldraphael@gmail.com>
2017-10-28 12:29:19 +0530

    Remove redundant IsFullScreenPromptBeingShown field

--- TicTacToe/GameState.cs
+++ TicTacToe/GameState.cs
@@ -8,9 +8,14 @@ namespace TicTacToe
 		public static TileValue Turn { get; set; } = TileValue.X;
 		public static TileValue Winner { get; set; } = TileValue.EMPTY;
 
+		/// <summary>
+		/// Set this to true to quit the application in the next loop
+		/// </summary>
+		/// <value><c>true</c> if should quit; otherwise, <c>false</c>.</value>
 		public static bool ShouldQuit { get; set; } = false;
+
+		/// <value><c>true</c> if is game over; otherwise, <c>false</c>.</value>
 		public static bool IsGameOver { get { return Winner != TileValue.EMPTY; }}
-		public static bool IsFullScreenPromptBeingShown { get; set; } = false;
 
 		public static void ToggleTurn()
 		{
@@ -25,7 +30,6 @@ namespace TicTacToe
 			Turn = TileValue.X;
 			Winner = TileValue.EMPTY;
 			ShouldQuit = false;
-			IsFullScreenPromptBeingShown = false;
 		}
 	}
 }
